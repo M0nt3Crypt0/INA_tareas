ad5e124d823a5ecaaf50fc5538d669dc23439a44
caesar-augustus <caesar.augustusqp@gmail.com>
2023-10-09 21:38:08 +0200

    Estructura inicial

---
+++ ShellyClassicAPI/main.py
@@ -0,0 +1,16 @@
+# Bibliotecas 
+import requests
+import json
+
+
+# Config
+SHELLY_PLUS_1_IP = "192.168.55.103"
+
+SHELLY_PLUS_HT_IP = "192.168.55.102"
+SHELLY_PLUS_DW_IP = "192.168.55.105"
+
+# Funciones
+
+
+
+# Main
