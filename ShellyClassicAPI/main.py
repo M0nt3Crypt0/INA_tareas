f54926a460fe7ca8b1e37b02d29401086cb41bea
caesar-augustus <caesar.augustusqp@gmail.com>
2023-10-09 23:06:44 +0200

    Funnciones basicas y main

--- ShellyClassicAPI/main.py
+++ ShellyClassicAPI/main.py
@@ -10,7 +10,26 @@ SHELLY_PLUS_HT_IP = "192.168.55.102"
 SHELLY_PLUS_DW_IP = "192.168.55.105"
 
 # Funciones
+def check_connection_to(ip):
+    url = f"http://{ip}/shelly"
+    response = requests.get(url)
+    return response.ok
 
+def get_value(ip, key):
+    url = f"http://{ip}/{key}"
+    response = requests.get(url)
+    return response.json
 
+def set_value(ip, key, payload):
+    url = f"http://{ip}/{key}" 
+    response = requests.post(url, json=payload)
+    return response.status_code == 200
 
 # Main
+def main():
+    # Si temp > 30 o puerta abierta enciende
+    if temp > 30 or open:
+        set_value(SHELLY_PLUS_1_IP, "relay/0", {"turn": "on"})
+    # else apaga
+    else:
+        set_value(SHELLY_PLUS_1_IP, "relay/0", {"turn": "off"})
