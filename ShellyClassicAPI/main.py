85a21198895be5b89db10853d5057b1869ec8be6
caesar <caesar.agustusqp@gmail.com>
2023-10-11 16:08:06 +0200

    Mas errores corregidos

--- ShellyClassicAPI/main.py
+++ ShellyClassicAPI/main.py
@@ -7,7 +7,7 @@ import sys
 SHELLY_PLUS_1_IP = "192.168.55.103"
 
 SHELLY_PLUS_HT_IP = "192.168.55.102"
-SHELLY_DW_IP = "192.168.55.105"
+SHELLY_DW_IP = "z"
 
 LATITUD = 39.46975
 LONGITUD = -0.37739
@@ -24,23 +24,34 @@ def post_api(device, path, payload):
     return response.status_code == 200
 
 def check_device_connection(device):
-    response = get_api(device, "/shelly")
+    try:
+        response = get_api(device, "/shelly")
+    except:
+        sys.exit("No connection")
     if not response.ok:
         sys.exit("Error no response")
+
+def shelly_plus_on():
+   status = get_api(SHELLY_PLUS_1_IP, "/relay/0").json()
+   if status["ison"] == False:
+       print("Enciende el relay")
+       get_api(SHELLY_PLUS_1_IP, "/relay/0?turn=on")
+
+
+def shelly_plus_off():
+   status = get_api(SHELLY_PLUS_1_IP, "/relay/0").json()
+   if status["ison"]:
+       print("Enciende el relay")
+       get_api(SHELLY_PLUS_1_IP, "/relay/0?turn=off")
 
 # Main
 def main():
     error = False
     error_msg = ""
 
-    
-    if error:
-        error_msg = "No se puede conectar a Shelly Plus HT"
-    if error:
-        error_msg = "No se puede conectar a Shelly DW"
-
 
     while not error:
+        
         # Lee temperatura interna
         check_device_connection(SHELLY_PLUS_HT_IP)
         response_json = get_api(SHELLY_PLUS_HT_IP, "/rpc/Temperature.GetStatus?id=0").json()
@@ -48,21 +59,22 @@ def main():
         print(temp)
 
         # Lee si la 'ventana' está abierta
-        check_device_connection(SHELLY_DW_IP)
-        response = get_api(SHELLY_DW_IP, "/")
-        response_json = json.load(response)
+        #check_device_connection(SHELLY_DW_IP)
+        #response = get_api(SHELLY_DW_IP, "/status")
+        #print(response.json())
+        #response_json = json.load(response)
         #open = response_json[]
 
         # Lee temperatura externa 
         # TODO
 
         # Si temp > 30 o puerta abierta enciende
-        if temp > 30 or open:
+        if temp > 20:
             check_device_connection(SHELLY_PLUS_1_IP)
-            post_api(SHELLY_PLUS_1_IP, "/relay/0", {"turn": "on"})
+            shelly_plus_on()
         # else apaga
         else:
-            post_api(SHELLY_PLUS_1_IP, "/relay/0", {"turn": "off"})
+            shelly_plus_off()
     
     sys.exit(error_msg)
 
